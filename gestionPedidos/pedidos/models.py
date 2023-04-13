ee5ffc4f2c5550249bbcb388634380525a3a6777
Hezitzaile <iban.argote@opendesuto.es>
2023-04-13 17:32:54 +0200

    creacion de models

--- gestionPedidos/pedidos/models.py
+++ gestionPedidos/pedidos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Pedido(models.Model):
+    codigo = models.CharField(max_length=100, unique=True)
+    fecha = models.DateField()
+    cif = models.CharField(max_length=100)
+    nombre_empresa = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    datos_contacto = models.CharField(max_length=100)
+    producto =  models.ManyToManyField(Producto) 
+    cantidad = models.IntegerField()
+    precio_total = models.FloatField()
+
+class Componente(models.Model):
+    codigo_referencia = models.CharField(max_length=100)
+    nombre_modelo = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+
+class Producto(models.Model):
+    referencia = models.CharField(max_length=100, unique=True)
+    precio = models.FloatField()
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    componentes = models.ManyToManyField(Componente)    	
