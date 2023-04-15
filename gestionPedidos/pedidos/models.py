46d8e5b9ddd7befe343e32106290bce02085022e
Hezitzaile <iban.argote@opendesuto.es>
2023-04-15 11:30:10 +0200

    Cambios models

--- gestionPedidos/pedidos/models.py
+++ gestionPedidos/pedidos/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-class Pedido(models.Model):
-    codigo = models.CharField(max_length=100, unique=True)
-    fecha = models.DateField()
-    cif = models.CharField(max_length=100)
-    nombre_empresa = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=100)
-    datos_contacto = models.CharField(max_length=100)
-    producto =  models.ManyToManyField(Producto) 
-    cantidad = models.IntegerField()
-    precio_total = models.FloatField()
-
 class Componente(models.Model):
-    codigo_referencia = models.CharField(max_length=100)
+    codigo_referencia = models.CharField(max_length=50, unique=True)
     nombre_modelo = models.CharField(max_length=100)
-    marca = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
 
 class Producto(models.Model):
-    referencia = models.CharField(max_length=100, unique=True)
-    precio = models.FloatField()
+    referencia = models.CharField(max_length=50, unique=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=100)
-    componentes = models.ManyToManyField(Componente)    	
+    descripcion = models.TextField()
+    categoria = models.CharField(max_length=50)
+    componentes = models.ManyToManyField(Componente)
+
+class Pedido(models.Model):
+    codigo_referencia = models.CharField(max_length=50, unique=True)
+    fecha = models.DateField()
+    cif_cliente = models.CharField(max_length=50)
+    nombre_empresa_cliente = models.CharField(max_length=100)
+    direccion_cliente = models.CharField(max_length=200)
+    datos_contacto_cliente = models.CharField(max_length=200)
+    producto_solicitado = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
